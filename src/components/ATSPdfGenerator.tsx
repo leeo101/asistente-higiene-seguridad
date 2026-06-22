9d47c53d37124fd9fa66dea28ced72bdc42ec61f
Leeo101 <enzorodriguez31@gmail.com>
2026-06-22 20:58:18 -0300

    fix: remove avoid-break from ATS category block

--- src/components/ATSPdfGenerator.tsx
+++ src/components/ATSPdfGenerator.tsx
@@ -343,7 +343,7 @@ export default function ATSPdfGenerator({ atsData, pdfElementId = 'pdf-content' 
             {categories.map((categoria, catIndex) => {
               const categoryItems = checklist.filter((item) => item.categoria === categoria);
               return (
-                <div key={catIndex} className="ats-pdf-category-block avoid-break">
+                <div key={catIndex} className="ats-pdf-category-block">
                   <div
                     style={{
                       background: '#e2e8f0',
